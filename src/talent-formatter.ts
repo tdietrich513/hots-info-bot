a4fe195a76d161c3c11479809753f70c7cf38943
Tom Dietrich <tdietrich@cardinalsolutions.com>
2018-02-01 16:16:06 -0500

    Group generics and list all heroes
    fixes: #5

--- src/talent-formatter.ts
+++ src/talent-formatter.ts
@@ -7,7 +7,15 @@ export class TalentFormatter {
             return `**${talent.name}** _${talent.hero} (Level ${talent.tier})_`;
         }
 
-        return `**${talent.name}** _(${talent.tier})_`;
+        return `**${talent.name}** _(Level ${talent.tier})_`;
+    }
+
+    static shortDupeText(talents: ITalentData[]): string {
+        let output = `**${talents[0].name}**`;
+        talents.forEach(t => {
+            output += `\n\t${t.hero} (Level ${t.tier})`;
+        });
+        return output;
     }
 
     static longText(talent: ITalentData, includeHero: boolean): string {
@@ -22,11 +30,33 @@ export class TalentFormatter {
         return output;
     }
 
+    static longDupeText(talents: ITalentData[]): string {
+        let output = `**${talents[0].name}**\n_${talents[0].description}_\n`;
+        talents.forEach(t => {
+            output += `\n\t${t.hero} (${t.tier})`;
+        });
+        output += "\n\n";
+        return output;
+    }
+
     static embed(embed: RichEmbed, talent: ITalentData, includeHero: boolean): RichEmbed {
         const talentDescription = `_${talent.description}_\n\n`;
 
         let talentTitle = `${talent.name}`;
-        if (includeHero) talentTitle += ` (${talent.hero})`;
+        if (includeHero) talentTitle += ` (${talent.hero} ${talent.tier})`;
+        else talentTitle += ` (${talent.tier})`;
+        embed.addField(talentTitle, talentDescription);
+
+        return embed;
+    }
+
+    static embedDupe(embed: RichEmbed, talents: ITalentData[]): RichEmbed {
+        let talentDescription = `_${talents[0].description}_\n`;
+        talents.forEach(t => {
+            talentDescription += `${t.hero} (${t.tier})\n`;
+        });
+
+        const talentTitle = `${talents[0].name}`;
         embed.addField(talentTitle, talentDescription);
 
         return embed;
